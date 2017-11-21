cc333ca1722d301ccf1973bd1b976dd4dbf9fb78
junaidahmed93 <junaidahmed93@live.com>
2017-11-21 10:56:19 +0500

    Ticket API Initiall

--- tutorial/quickstart/models.py
+++ tutorial/quickstart/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Ticket(models.Model):
-    SEVERITY = (
+    print('auth.user')
+    SEVERITY = ( 
         (1,'Major'),
         (2,'Minor')
     )
-    created = models.DateTimeField(auto_now_add=True)
-    assignee = models.CharField(max_length=90, blank=True)
-    reporter = models.CharField(max_length=100, blank=True)
-    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)    
+    reporter = models.ForeignKey('auth.User', related_name='reporter') 
+    assignee = models.ForeignKey('auth.User', related_name='assignee')
+    description = models.TextField() 
     done_date = models.DateTimeField(null=True)
     severity = models.CharField(max_length=100,choices=SEVERITY,default='')
     is_done = models.BooleanField(default= False)
